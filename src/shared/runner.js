b956953b8acfd16edc367d26a4594f6c72eedfd1
Travis Sutherland <Travis.Sutherland@vsp.com>
2019-09-12 12:52:38 -0700

    new url

--- src/shared/runner.js
+++ src/shared/runner.js
@@ -25,7 +25,7 @@ function runHello(isServer, logFunction) {
         transport: {
             // url: "ws://localhost:8101/",
             // url: "wss://rsocket-innovate.herokuapp.com/ws",
-            url: "wss://8bfde240.ngrok.io",
+            url: "wss://7b578061.ngrok.io",
         },
     });
 
